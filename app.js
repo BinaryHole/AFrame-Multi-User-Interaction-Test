ae9db5b582e741bb479a139c24dd4d79c7c51031
Mark McGregor <mark.mcgregor.contact@gmail.com>
2021-02-20 12:32:18 -0500

    Add simple multiplayer functionality

--- app.js
+++ app.js
@@ -18,21 +18,30 @@ app.get('/', (req, res) => {
 var players = [];
 
 // set up socket.io session
-io.on('connection', (socket) => {
+io.on('connection', socket => {
 
   // when the client connects
   onConnect(socket);
 
   // when the client disconnects
-  socket.on('disconnect', () => {
+  socket.on('disconnect', (reason) => {
     onDisconnect(socket);
   });
+
   // when the player moves
-  socket.on('moved', (newPosition) => {
-    for (i in players) {
+  socket.on('moved', (data) => {
+    // loop through each player in the list
+    for (var i = 0; i < players.length; i++) {
+      // if this is the player that has moved (the socket)
       if (players[i].id == socket.id) {
-        i.position = newPosition;
-        console.log('Player\t' + players[i].id + ' moved, new position:\t' + newPosition);
+        // update the position of the player
+        players[i].position = data.newPosition;
+
+        console.log('Player\t%s moved, new position:\t%s',
+          players[i].id, players[i].position);
+
+        // emit the updatePlayers socket event
+        socket.broadcast.emit('updatePlayers', {players: players});
       }
     }
   });
@@ -41,23 +50,37 @@ io.on('connection', (socket) => {
 // called when a new client connects
 onConnect = (socket) => {
   // add the new client id to the list of connections
-  players.push({id: socket.id, name: '', position: [0, 0, 0]});
-  console.log('Connection ' + socket.id + ' added. \t\t# of connections: ' + players.length);
+  players.push({id: socket.id, name: '', position: {x:0, y:0, z:0}});
 
-  // invoke the usersChanged event
-  io.sockets.emit('usersChanged', {users: players});
+  // log which player was added and the list of players
+  console.log('Connection ' + socket.id + ' added. \t\t# of connections: '
+    + players.length);
+  console.log(players);
+
+  // invoke the spawnInitialPlayers event this socket
+  socket.emit('spawnInitialPlayers', {players: players});
+
+  // invoke the playerJoined event to all other socket
+  socket.broadcast.emit('playerJoined', {player:
+    {id: socket.id, name: '', position: [0, 0, 0]}});
 }
 
 // called when a client disconnects
 onDisconnect = (socket) => {
   // remove the client's id from the list of connections
-  for (i in players) {
+  for (var i = 0; i < players.length; i++) {
     if (players[i].id == socket.id) {
-      players.splice(players[i], 1);
-      console.log('Connection ' + socket.id + ' removed. \t# of connections: ' + players.length);
+      // remove the player from the list of players
+      players.splice(i, 1);
+
+      // log which player was removed and the list of players left
+      console.log('Connection ' + socket.id + ' removed. \t# of connections: '
+        + players.length);
+      console.log(players);
+
+      // invoke the usersChanged event
+      io.emit('playerQuit', {playerId: socket.id});
     }
   }
 
-  // invoke the usersChanged event
-  io.sockets.emit('usersChanged', {users: players});
 }
